85be2aa96f4f50ef8867f1e6f3ba04e96b7df04e
Otávio <gutsgt1@gmail.com>
2023-03-08 11:49:50 -0300

    atualizacao

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -3,56 +3,33 @@
 namespace App\Providers;
 
 use App\Actions\Jetstream\DeleteUser;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Fortify\Fortify;
 use Laravel\Jetstream\Jetstream;
 
 class JetstreamServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
-     * @return void
      */
-    public function register()
+    public function register(): void
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-
-        Fortify::loginView(function(){
-            return view('auth.login');
-        });
-
         $this->configurePermissions();
 
         Jetstream::deleteUsersUsing(DeleteUser::class);
-
-        Fortify::authenticateUsing(function (Request $request) {
-            $request->flashExcept('password');
-            $user = User::where('email', $request->email)->first();
-            if ($user && Hash::check($request->password, $user->password)) {
-                return $user;
-            }
-        });
     }
 
     /**
      * Configure the permissions that are available within the application.
-     *
-     * @return void
      */
-    protected function configurePermissions()
+    protected function configurePermissions(): void
     {
         Jetstream::defaultApiTokenPermissions(['read']);
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,11 +11,39 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+
+//     // Teste de envio de informações para a view
+//     // $nome = "Otávio";
+//     // $idade = 21;
+//     // return view('welcome', ['nome'=>$nome, 'idade'=>$idade, 'profissao'=>"Programador"]);
+//     // Coloque esse trecho de código na view
+//     // <p>{{ $nome }} - {{ $idade }} anos, trabalha como {{ $profissao }}</p>
+
+//     return view('welcome');
+// });
+
+// Route::get('/products', [ProductController::class, 'search']);
+// Route::get('/product/{id?}', [ProductController::class, 'view']);
+
+Route::get('/', [EventController::class, 'index']);
+
+Route::get('/events/list', [EventController::class, 'list']);
+Route::get('/events/manage', [EventController::class, 'manage'])->middleware('auth');
+Route::get('/events/{event:title}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store'])->middleware('auth');
+Route::delete('/events/{event:title}', [EventController::class, 'destroy'])->middleware('auth');
+Route::put('/events/update/{event:title}', [EventController::class, 'update'])->middleware('auth');
+
+Route::get('/contact', [ContactController::class, 'index']);
+
+Route::get('/my_events', [EventController::class, 'myEvents'])->middleware('auth');
+Route::get('/schedule', [EventController::class, 'schedule'])->middleware('auth');
+
+Route::post('/events/join/{event:title}', [EventController::class, 'joinEvent'])->middleware('auth');
+Route::delete('/events/leave/{event:title}', [EventController::class, 'leaveEvent'])->middleware('auth');
